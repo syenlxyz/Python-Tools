53913ca352cdf77b24f3175feb378f7b1396fa21
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-09 18:00:15 +0800

    Update run-duplex.py

--- Print-PDF/run-duplex.py
+++ Print-PDF/run-duplex.py
@@ -1,14 +1,15 @@
 from alive_progress import alive_it
 from datetime import datetime
 from pathlib import Path
+from pypdf import PdfReader, PdfWriter
 from win32com.client import Dispatch
 from win32print import SetDefaultPrinter
 import psutil
 
 PageOption = {
     'PDAllPages': -3,
-    'PDEvenPagesOnly': -4,
-    'PDOddPagesOnly': -5
+    'PDOddPagesOnly': -4,
+    'PDEvenPagesOnly': -5
 }
 
 def run():
@@ -25,26 +26,47 @@ def run():
         'dual_line': True
     }
     
-    results = alive_it(
-        file_list, 
-        len(file_list), 
-        finalize=lambda bar: bar.text('Printing PDF Document: done'),
-        **options
-    )
+    target_printer = 'EPSON L3150 Series'
+    SetDefaultPrinter(target_printer)
     
     process_list = list(psutil.process_iter())
     for process in process_list:
         if process.name() == 'Acrobat.exe':
             process.terminate()
     
-    target_printer = 'EPSON L3150 Series'
-    SetDefaultPrinter(target_printer)
-    
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Printing PDF Document (Even Pages Only): done'),
+        **options
+    )
     for file_path in results:
-        results.text(f'Printing PDF Document: {file_path.name}')
-        print_pdf(file_path, PageOption['PDEvenPagesOnly'])
-        import time
-        time.sleep(1)
+        results.text(f'Printing PDF Document (Even Pages Only): {file_path.name}')
+        
+        reader = PdfReader(file_path)
+        num_page = reader.get_num_pages()
+        if num_page % 2:
+            writer = PdfWriter(reader)
+            writer.add_blank_page()
+            
+            temp_path = input_path / 'temp.pdf'
+            with open(temp_path, 'wb'):
+                writer.write(temp_path)
+            
+            print_pdf(temp_path, PageOption['PDEvenPagesOnly'])
+            temp_path.unlink()
+        else:
+            print_pdf(file_path, PageOption['PDEvenPagesOnly'])
+    
+    input('Press ENTER to continue...')
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Printing PDF Document (Odd Pages Only): done'),
+        **options
+    )
+    for file_path in results:
+        results.text(f'Printing PDF Document (Odd Pages Only): {file_path.name}')
         print_pdf(file_path, PageOption['PDOddPagesOnly'])
 
 def print_pdf(file_path, iPageOption):
