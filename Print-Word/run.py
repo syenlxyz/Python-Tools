c1dbe3993ebd5bad107b781029ded5510023875f
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-09 20:30:19 +0800

    Update run.py

--- Print-Word/run.py
+++ Print-Word/run.py
@@ -2,6 +2,7 @@ from alive_progress import alive_it
 from datetime import datetime
 from pathlib import Path
 from win32com.client import Dispatch
+from win32print import SetDefaultPrinter
 
 WdPrintOutPages = {
     'AllPages': 0,
@@ -21,6 +22,9 @@ def run():
         'receipt_text': True,
         'dual_line': True
     }
+    
+    target_printer = 'EPSON L3150 Series'
+    SetDefaultPrinter(target_printer)
     
     file_list = list(input_path.glob('*.doc')) + list(input_path.glob('*.docx'))
     results = alive_it(
