dc0e673dff8407e3d8123b7846272851fc084ebf
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-21 16:06:42 +0800

    Create run.py

---
+++ Count-PDF-Pages/run.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pathlib import Path
+from pypdf import PdfReader
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    file_list = list(input_path.glob('*.pdf'))
+    for file_path in file_list:
+        num_page = get_num_page(file_path)
+        print(f'{file_path.name}: {num_page}')
+
+def get_num_page(file_path):
+    reader = PdfReader(file_path)
+    num_page = reader.get_num_pages()
+    return num_page
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
