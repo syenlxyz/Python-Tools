c268b1439cc967ae65f373ab25a8590661998f7e
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-21 21:54:33 +0800

    Update run-even.py

--- Print-PDF/run-even.py
+++ Print-PDF/run-even.py
@@ -6,7 +6,7 @@ from win32com.client import Dispatch
 from win32print import SetDefaultPrinter
 import psutil
 
-PageOption = {
+iPageOption = {
     'PDAllPages': -3,
     'PDOddPagesOnly': -4,
     'PDEvenPagesOnly': -5
@@ -28,11 +28,6 @@ def run():
     target_printer = 'EPSON L3150 Series'
     SetDefaultPrinter(target_printer)
     
-    process_list = list(psutil.process_iter())
-    for process in process_list:
-        if process.name() == 'Acrobat.exe':
-            process.terminate()
-    
     file_list = list(input_path.glob('*.pdf'))
     results = alive_it(
         file_list, 
@@ -40,48 +35,47 @@ def run():
         finalize=lambda bar: bar.text('Printing PDF (Even Pages Only): done'),
         **options
     )
-    for file_path in results:
-        results.text(f'Printing PDF Document (Even Pages Only): {file_path.name}')
-        
-        reader = PdfReader(file_path)
-        num_page = reader.get_num_pages()
-        if num_page % 2:
-            writer = PdfWriter(reader)
-            writer.add_blank_page()
-            
-            temp_path = input_path / 'temp.pdf'
-            with open(temp_path, 'wb'):
-                writer.write(temp_path)
-            
-            print_pdf(temp_path, PageOption['PDEvenPagesOnly'])
-            temp_path.unlink()
-        else:
-            print_pdf(file_path, PageOption['PDEvenPagesOnly'])
     
-
-def print_pdf(file_path, iPageOption):
-    app = Dispatch('AcroExch.App')
-    app.Hide()
-    
-    avDoc = Dispatch('AcroExch.AVDoc')
-    avDoc.Open(file_path, file_path)
-    
-    pdDoc = avDoc.GetPDDoc()
-    num_page = pdDoc.GetNumPages()
+    process_list = list(psutil.process_iter())
+    for process in process_list:
+        if process.name() == 'Acrobat.exe':
+            process.terminate()
     
     params = {
         'nFirstPage': 0,
-        'nLastPage': num_page - 1,
+        'nLastPage': None,
         'nPSLevel': 3,
         'bBinaryOk': False,
         'bShrinkToFit': True,
         'bReverse': True,
         'bFarEastFontOpt': False,
         'bEmitHalftones': False,
-        'iPageOption': iPageOption
+        'iPageOption': iPageOption['PDEvenPagesOnly']
     }
-    avDoc.PrintPagesEx(**params)
-    avDoc.Close(True)
+    
+    app = Dispatch('AcroExch.App')
+    app.Hide()
+    avDoc = Dispatch('AcroExch.AVDoc')
+    for file_path in results:
+        results.text(f'Printing PDF Document (Even Pages Only): {file_path.name}')
+        reader = PdfReader(file_path)
+        num_page = reader.get_num_pages()
+        if num_page % 2:
+            writer = PdfWriter(reader)
+            writer.add_blank_page()
+            temp_path = input_path / 'temp.pdf'
+            with open(temp_path, 'wb'):
+                writer.write(temp_path)
+            avDoc.Open(temp_path, temp_path)
+            params['nLastPage'] = num_page
+            avDoc.PrintPagesEx(**params)
+            avDoc.Close(True)
+            temp_path.unlink()
+        else:
+            avDoc.Open(file_path, file_path)
+            params['nLastPage'] = num_page - 1
+            avDoc.PrintPagesEx(**params)
+            avDoc.Close(True)
     
     process_list = list(psutil.process_iter())
     for process in process_list:
