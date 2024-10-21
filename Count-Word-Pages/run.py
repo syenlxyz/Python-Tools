3ffa8ed05992612cb90b909ab8aad3bb95d87dd1
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-21 20:12:38 +0800

    Update run.py

--- Count-Word-Pages/run.py
+++ Count-Word-Pages/run.py
@@ -8,7 +8,7 @@ def run():
         input_path.mkdir()
     
     file_list = []
-    path_list = list(input_path.iterdir())
+    path_list = list(input_path.glob('**/*'))
     for path in path_list:
         if path.suffix in ['.doc', '.docx']:
             file_list.append(path)
@@ -16,15 +16,11 @@ def run():
     wrd = Dispatch('Word.Application')
     wrd.Visible = False
     for file_path in file_list:
-        num_page = get_num_page(wrd, file_path)
+        wrd.Documents.Open(str(file_path))
+        num_page = wrd.ActiveDocument.ComputeStatistics(Statistic=2)
+        wrd.ActiveDocument.Close(SaveChanges=False)
         print(f'{file_path.name}: {num_page}')
     wrd.Quit()
-
-def get_num_page(wrd, file_path):
-    wrd.Documents.Open(str(file_path))
-    num_page = wrd.ActiveDocument.ComputeStatistics(Statistic=2)
-    wrd.ActiveDocument.Close(SaveChanges=False)
-    return num_page
 
 if __name__ == '__main__':
     print(f'Running {Path(__file__).parent.name}')
@@ -33,7 +29,3 @@ if __name__ == '__main__':
     end_time = datetime.now()
     run_time = end_time - start_time
     print(f'Execution time: {run_time}')
-
-
-
-    
