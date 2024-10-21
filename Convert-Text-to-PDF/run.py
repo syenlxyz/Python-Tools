caca018785e478e5863663426ec799ca08235c6e
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-21 22:23:37 +0800

    Create run.py

---
+++ Convert-Text-to-PDF/run.py
@@ -0,0 +1,63 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+from win32com.client import Dispatch
+import psutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        output_path.mkdir()
+    else:
+        file_list = list(output_path.iterdir())
+        for file_path in file_list:
+            file_path.unlink()
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    file_list = list(input_path.glob('**/*.txt'))
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Converting Text to PDF: done'),
+        **options
+    )
+    
+    for file_path in results:
+        results.text(f'Converting File to PDF: {file_path.name}')
+        file_to_pdf(file_path, output_path)
+
+def file_to_pdf(file_path, output_path):
+    app = Dispatch('AcroExch.App')
+    app.Hide()
+    
+    avDoc = Dispatch('AcroExch.AVDoc')
+    avDoc.Open(file_path, file_path)
+    pdDoc = avDoc.GetPDDoc()
+    
+    pdf_path = output_path / file_path.with_suffix('.pdf').name
+    pdDoc.Save(1, pdf_path)
+    avDoc.Close(True)
+    
+    process_list = list(psutil.process_iter())
+    for process in process_list:
+        if process.name() == 'Acrobat.exe':
+            process.terminate()
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
