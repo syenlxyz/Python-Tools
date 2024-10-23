848355a4f2fcdcb974eaa3b9c4e82143da5aebd0
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-23 23:53:36 +0800

    Update run.py

--- Project-File/Rename-Files/run.py
+++ Project-File/Rename-Files/run.py
@@ -1,3 +1,4 @@
+from alive_progress import alive_it
 from datetime import datetime
 from pathlib import Path
 import shutil
@@ -9,12 +10,57 @@ def run():
     
     output_path = Path.cwd() / 'output'
     if not output_path.is_dir():
-        output_path.mkdir()
+        shutil.copytree(str(input_path), str(output_path))
     else:
         shutil.rmtree(str(output_path))
-        output_path.mkdir()
+        shutil.copytree(str(input_path), str(output_path))
     
+    file_list = []
+    folder_list = []
+    path_list = list(output_path.iterdir())
+    for path in path_list:
+        if path.is_file():
+            file_list.append(path)
+        if path.is_dir():
+            folder_list.append(path)
     
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Renaming File: done'),
+        **options
+    )
+    for index, file_path in enumerate(results):
+        results.text(f'Renaming File: {file_path.name}')
+        num_file = len(file_list)
+        num_digit = len(str(num_file))
+        prefix = str(index + 1).zfill(num_digit)
+        target_path = file_path.parent / f'{prefix}_{file_path.name}'
+        file_path.rename(target_path)
+    
+    for folder_path in folder_list:
+        file_list = list(folder_path.iterdir())
+        results = alive_it(
+            file_list, 
+            len(file_list), 
+            finalize=lambda bar: bar.text(f'Renaming File for {folder_path.relative_to(output_path)}: done'),
+            **options
+        )
+        for index, file_path in enumerate(results):
+            results.text(f'Renaming File for {folder_path.relative_to(output_path)}')
+            num_file = len(file_list)
+            num_digit = len(str(num_file))
+            prefix = str(index + 1).zfill(num_digit)
+            target_path = file_path.parent / f'{prefix}_{file_path.name}'
+            file_path.rename(target_path)
 
 if __name__ == '__main__':
     print(f'Running {Path(__file__).parent.name}')
