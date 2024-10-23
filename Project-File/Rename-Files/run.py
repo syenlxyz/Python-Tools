a11550b33ded991d460e2a5c0043748f43f93801
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-23 18:43:54 +0800

    Update run.py

---
+++ Project-File/Rename-Files/run.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from pathlib import Path
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
