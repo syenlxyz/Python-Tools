b89efe3c48fb6e57a6fd1681282799cc4ef429a4
Syen Yang Lu <syenlxyz@gmail.com>
2024-10-23 18:44:48 +0800

    Update run.py

---
+++ Project-File/Split-Files/run.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        output_path.mkdir()
+    else:
+        shutil.rmtree(str(output_path))
+        output_path.mkdir()
+    
+    
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
