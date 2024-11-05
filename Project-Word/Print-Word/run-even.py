837b4827d9bcef6fcb82dcbfc73a575479054cc5
Syen Yang Lu <syenlxyz@gmail.com>
2024-11-05 21:56:36 +0800

    Update run-even.py

--- Project-Word/Print-Word/run-even.py
+++ Project-Word/Print-Word/run-even.py
@@ -2,7 +2,6 @@ from alive_progress import alive_it
 from datetime import datetime
 from pathlib import Path
 from win32com.client import Dispatch
-from win32print import SetDefaultPrinter
 
 WdPrintOutPages = {
     'AllPages': 0,
@@ -22,9 +21,6 @@ def run():
         'receipt_text': True,
         'dual_line': True
     }
-    
-    target_printer = 'EPSON L3150 Series'
-    SetDefaultPrinter(target_printer)
     
     file_list = []
     path_list = list(input_path.glob('**/*'))
