9e12e8d8ee432ed0ac478a3ef283f352bfedfd4b
Syen Yang Lu <syenlxyz@gmail.com>
2024-11-05 21:56:32 +0800

    Update run.py

--- Project-PDF/Print-PDF/run.py
+++ Project-PDF/Print-PDF/run.py
@@ -2,7 +2,6 @@ from alive_progress import alive_it
 from datetime import datetime
 from pathlib import Path
 from win32com.client import Dispatch
-from win32print import SetDefaultPrinter
 import psutil
 
 iPageOption = {
@@ -23,9 +22,6 @@ def run():
         'receipt_text': True,
         'dual_line': True
     }
-    
-    target_printer = 'EPSON L3150 Series'
-    SetDefaultPrinter(target_printer)
     
     file_list = list(input_path.glob('**/*.pdf'))
     results = alive_it(
