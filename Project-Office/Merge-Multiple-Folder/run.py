3a3ebbf04dad361dd4854489f301f953ba02a1a6
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-03 15:29:44 +0800

    Merge-Multiple-Folder

---
+++ Project-Office/Merge-Multiple-Folder/run.py
@@ -0,0 +1,50 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        output_path.mkdir()
+    else:
+        shutil.rmtree(str(output_path))
+        output_path.mkdir()
+    
+    file_list = []
+    path_list = list(input_path.glob('**/*'))
+    for path in path_list:
+        if path.is_file():
+            file_list.append(path)
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Copying File: done'),
+        **options
+    )
+    
+    for file_path in results:
+        results.text(f'Copying File: {file_path.name}')
+        target_path = output_path / file_path.name
+        shutil.copy2(str(file_path), str(target_path))
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
