17a23a604290d6980c052cb65095958d2ad6c5cc
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-03 15:05:41 +0800

    Update run.py

--- Project-Media/Rename-TV-Episode/run.py
+++ Project-Media/Rename-TV-Episode/run.py
@@ -27,13 +27,13 @@ def run():
     results = alive_it(
         folder_list,
         len(folder_list),
-        finalize=lambda bar: bar.text('Renaming TV episodes: done'),
+        finalize=lambda bar: bar.text('Renaming TV Episode: done'),
         **options
     )
     
     for folder_path in results:
         folder_name = folder_path.name
-        results.text(f'Renaming TV episodes: {folder_name}')
+        results.text(f'Renaming TV Episode: {folder_name}')
         file_list = list(folder_path.glob('*.mp4'))
         for file_path in file_list:
             file_name = file_path.name
