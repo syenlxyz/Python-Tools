b609ec44646fa93a220f3ad07f97a3e06fcece47
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-03 16:26:11 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,49 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+from string import ascii_lowercase
+
+def run():
+    input_path = Path.cwd()
+    output_path = Path.cwd() / 'README.md'
+    folder_list = list(input_path.glob('Project-*'))
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+
+    title = '# Python-Tools'
+    base_url = 'https://github.com/syenlxyz/Python-Tools/tree/main/'
+    
+    lines = []
+    lines.append(title)
+    for index, folder_path in enumerate(folder_list):
+        lines.append(f'### {index + 1}. {folder_path.name}')
+        file_list = list(folder_path.iterdir())
+        results = alive_it(
+            file_list, 
+            len(file_list), 
+            finalize=lambda bar: bar.text(f'Creating URL for {folder_path.name}: done'),
+            **options
+        )
+        for index, file_path in enumerate(results):
+            results.text(f'Creating URL for {folder_path.name}: {file_path.name}')
+            target_path = file_path.relative_to(input_path)
+            target_url = base_url + str(target_path)
+            lines.append(f'{ascii_lowercase[index]}. {file_path.name}: {target_url}')
+    
+    text = '\n'.join(lines)
+    with open(output_path, 'w') as file:
+        file.write(text)
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
