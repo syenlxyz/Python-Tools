ea70b442ca04ffe070d6e15e46a6fc4751e3036d
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-03 15:09:25 +0800

    Update run-lite.py

--- Project-Media/Download-YouTube-MP4/run-lite.py
+++ Project-Media/Download-YouTube-MP4/run-lite.py
@@ -24,12 +24,12 @@ def run():
     results = alive_it(
         playlist,
         len(playlist),
-        finalize=lambda bar: bar.text('Downloading MP4: done'),
+        finalize=lambda bar: bar.text('Downloading YouTube MP4 (Lite Version): done'),
         **options
     )
     
     for url in results:
-        results.text(f'Downloading MP4: {url}')
+        results.text(f'Downloading YouTube MP4 (Lite Version): {url}')
         yt = YouTube(url)
         yt.streams \
         .filter(progressive=True, file_extension='mp4') \
