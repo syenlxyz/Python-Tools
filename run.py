aab650a710012ef6e514fb8960baa3a43399e7d4
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-03 16:45:16 +0800

    Update run.py

--- run.py
+++ run.py
@@ -31,8 +31,8 @@ def run():
         for index, file_path in enumerate(results):
             results.text(f'Creating URL for {folder_path.name}: {file_path.name}')
             lines.append(f'{ascii_lowercase[index]}. {file_path.name}:')
-            base_url = 'https://github.com/syenlxyz/Python-Tools/tree/main/'
-            target_url = base_url + str(file_path.relative_to(input_path))
+            base_url = 'https://github.com/syenlxyz/Python-Tools/tree/main'
+            target_url = '/'.join([base_url, folder_path.name, file_path.name])
             lines.append(target_url)
     
     text = '\n\n'.join(lines)
