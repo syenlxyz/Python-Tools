de8d1d4f3abfc2bbd39d6a006853d2e59a7de6cf
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-06 21:20:08 +0800

    Update run.py

--- Project-Office/Compress-7Z-File/run.py
+++ Project-Office/Compress-7Z-File/run.py
@@ -24,7 +24,15 @@ def run():
         'dual_line': True
     }
     
-    folder_list = list(input_path.iterdir())
+    file_list = []
+    folder_list = []
+    path_list = list(input_path.iterdir())
+    for path in path_list:
+        if path.is_file():
+            file_list.append(path)
+        if path.is_dir():
+            folder_list.append(path)
+    
     results = alive_it(
         folder_list, 
         len(folder_list), 
@@ -36,6 +44,10 @@ def run():
         results.text(f'Compressing 7Z File: {folder_path.name}')
         target_path = output_path / folder_path.name
         subprocess.run(f'7z a -bso0 -bsp0 "{target_path}" "{folder_path}"')
+    else:
+        target_path = output_path / input_path.name
+        file_list = ' '.join([str(path) for path in file_list])
+        subprocess.run(f'7z a -bso0 -bsp0 "{target_path}" "{file_list}"')
 
 if __name__ == '__main__':
     print(f'Running {Path(__file__).parent.name}')
