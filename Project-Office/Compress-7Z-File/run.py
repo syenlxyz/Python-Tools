26dc03e7c3f54ac7486eea920370e68b09be21b0
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-06 21:53:08 +0800

    Update run.py

--- Project-Office/Compress-7Z-File/run.py
+++ Project-Office/Compress-7Z-File/run.py
@@ -42,8 +42,7 @@ def run():
     for file_path in results:
         results.text(f'Compressing 7Z File for {input_path.name}: {file_path.name}')
         target_path = output_path / file_path.name
-        for file_path in file_list:
-            subprocess.run(f'7z a -bso0 -bsp0 "{target_path}" "{file_path}"')
+        subprocess.run(f'7z a -bso0 -bsp0 "{target_path}" "{file_path}"')
     
     for folder_path in folder_list:
         file_list = list(folder_path.iterdir())
