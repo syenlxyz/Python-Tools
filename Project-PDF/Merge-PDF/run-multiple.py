e295912fc850bfab679c53d2ffa26abc50c5ff57
Syen Yang Lu <syenlxyz@gmail.com>
2025-01-09 16:55:35 +0800

    Update run-multiple.py

--- Project-PDF/Merge-PDF/run-multiple.py
+++ Project-PDF/Merge-PDF/run-multiple.py
@@ -32,15 +32,24 @@ def run():
     
     file_list = list(input_path.glob('*.pdf'))
     if file_list:
+        num_file = len(file_list)
+        num_top = num_file // 2 + 1
+        num_bottom = num_file // 2
+        top_list = [i * 2 for i in range(num_top)]
+        bottom_list = [i * 2 + 1 for i in range(num_bottom)]
+        order_list = top_list + bottom_list
+        index_list = [order_list.index(value) for value in range(num_file)]
+        file_list = [file_list[index] for index in index_list]
+        
         results = alive_it(
             file_list, 
             len(file_list), 
-            finalize=lambda bar: bar.text(f'Merging PDF for {input_path.name}: done'),
+            finalize=lambda bar: bar.text(f'Merging PDF in Multiple for {input_path.name}: done'),
             **options
         )
         merger = PdfWriter()
         for file_path in results:
-            results.text(f'Merging PDF for {input_path.name}: {file_path.name}')
+            results.text(f'Merging PDF in Multiple for {input_path.name}: {file_path.name}')
             merger.append(file_path)
         target_path = output_path / input_path.with_suffix('.pdf').name
         merger.write(target_path)
@@ -48,21 +57,29 @@ def run():
     
     for folder_path in folder_list:
         file_list = list(folder_path.glob('*.pdf'))
-        if not file_list:
-            continue
-        results = alive_it(
-            file_list, 
-            len(file_list), 
-            finalize=lambda bar: bar.text(f'Merging PDF for {folder_path.name}: done'),
-            **options
-        )
-        merger = PdfWriter()
-        for file_path in results:
-            results.text(f'Merging PDF for {folder_path.name}: {file_path.name}')
-            merger.append(file_path)
-        target_path = output_path / folder_path.with_suffix('.pdf').name
-        merger.write(target_path)
-        merger.close()
+        if file_list:
+            num_file = len(file_list)
+            num_top = num_file // 2 + 1
+            num_bottom = num_file // 2
+            top_list = [i * 2 for i in range(num_top)]
+            bottom_list = [i * 2 + 1 for i in range(num_bottom)]
+            order_list = top_list + bottom_list
+            index_list = [order_list.index(value) for value in range(num_file)]
+            file_list = [file_list[index] for index in index_list]
+            
+            results = alive_it(
+                file_list, 
+                len(file_list), 
+                finalize=lambda bar: bar.text(f'Merging PDF in Multiple for {folder_path.name}: done'),
+                **options
+            )
+            merger = PdfWriter()
+            for file_path in results:
+                results.text(f'Merging PDF in Multiple for {folder_path.name}: {file_path.name}')
+                merger.append(file_path)
+            target_path = output_path / folder_path.with_suffix('.pdf').name
+            merger.write(target_path)
+            merger.close()
 
 if __name__ == '__main__':
     print(f'Running {Path(__file__).parent.name}')
