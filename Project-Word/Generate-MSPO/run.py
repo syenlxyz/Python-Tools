95dc9b2ed58b8ffcd820d13c99c246703d7f6006
Syen Yang Lu <syenlxyz@gmail.com>
2025-09-10 01:26:24 +0800

    Create Generate-MSPO

---
+++ Project-Word/Generate-MSPO/run.py
@@ -0,0 +1,64 @@
+from alive_progress import alive_it
+from datetime import datetime
+from docx import Document
+from pathlib import Path
+from send2trash import send2trash
+import re
+import shutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        shutil.copytree(str(input_path), str(output_path))
+    else:
+        send2trash(output_path)
+        shutil.copytree(str(input_path), str(output_path))
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    file_list = list(output_path.glob('**/*.docx'))
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text(f'Generating MSPO: done'),
+        **options
+    )
+    
+    data = {
+        'COMPANY': 'SIN KEE CHIANG ESTATE SDN BHD',
+        'NUMBER': '257853-P',
+        'PREPARE': 'Poh Moi Hua',
+        'APPROVE': 'Wong Gin Poh',
+        'DATE': '06/01/2025'
+    }
+
+    for file_path in results:
+        results.text(f'Generating MSPO: {file_path.name}')
+        doc = Document(file_path)
+        for paragraph in doc.paragraphs:
+            pattern = r'\$\{(.*?)\}'
+            result = re.findall(pattern, paragraph.text)
+            if result:
+                key = result[0]
+                value = data[key]
+                run = paragraph.runs[0]
+                run.text = run.text.replace('${%s}' % (key), value)
+        doc.save(file_path)
+
+if __name__ == '__main__':
+    print(f'Running {Path(__file__).parent.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
