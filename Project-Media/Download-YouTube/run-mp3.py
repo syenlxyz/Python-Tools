a1af388f6932175235666bbb0dd81e05584e2c68
Syen Yang Lu <syenlxyz@gmail.com>
2025-09-20 12:40:47 +0800

    Update run-mp3.py

--- Project-Media/Download-YouTube/run-mp3.py
+++ Project-Media/Download-YouTube/run-mp3.py
@@ -27,12 +27,12 @@ def run():
     results = alive_it(
         playlist,
         len(playlist),
-        finalize=lambda bar: bar.text('Downloading YouTube MP3: done'),
+        finalize=lambda bar: bar.text('Processing: done'),
         **options
     )
     
     for url in results:
-        results.text(f'Downloading YouTube MP3: {url}')
+        results.text(f'Processing: {url}')
         yt = YouTube(url, use_po_token=True, po_token_verifier=po_token_verifier)
         audio_file = Path(
             yt.streams
@@ -80,7 +80,9 @@ def po_token_verifier():
     return po_token
 
 if __name__ == '__main__':
-    print(f'Running {Path(__file__).parent.name}')
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
     start_time = datetime.now()
     run()
     end_time = datetime.now()
