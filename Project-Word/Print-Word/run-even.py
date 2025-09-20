ae149b9da9f058765110117e07229d5f83d2d4f9
Syen Yang Lu <syenlxyz@gmail.com>
2025-09-20 12:43:29 +0800

    Update run-even.py

--- Project-Word/Print-Word/run-even.py
+++ Project-Word/Print-Word/run-even.py
@@ -32,7 +32,7 @@ def run():
     results = alive_it(
         file_list, 
         len(file_list), 
-        finalize=lambda bar: bar.text('Printing Word (Even Pages Only): done'),
+        finalize=lambda bar: bar.text('Processing: done'),
         **options
     )
     
@@ -60,7 +60,7 @@ def run():
     wrd.Visible = False
     wrd.Options.PrintReverse = True
     for file_path in results:
-        results.text(f'Printing Word (Even Pages Only): {file_path.name}')
+        results.text(f'Processing: {file_path.name}')
         wrd.Documents.Open(str(file_path))
         num_page = wrd.ActiveDocument.ComputeStatistics(Statistic=2)
         if num_page % 2:
@@ -73,7 +73,9 @@ def run():
     wrd.Quit()
 
 if __name__ == '__main__':
-    print(f'Running {Path(__file__).parent.name}')
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
     start_time = datetime.now()
     run()
     end_time = datetime.now()
