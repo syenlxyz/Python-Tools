806e3988dbd91b63c1a770530768ba6817498953
Syen Yang Lu <syenlxyz@gmail.com>
2025-09-20 12:42:16 +0800

    Update run.py

--- Project-PDF/Convert-File-to-PDF/run.py
+++ Project-PDF/Convert-File-to-PDF/run.py
@@ -36,7 +36,7 @@ def run():
     results = alive_it(
         file_list, 
         len(file_list), 
-        finalize=lambda bar: bar.text('Converting File to PDF: done'),
+        finalize=lambda bar: bar.text('Processing: done'),
         **options
     )
     
@@ -49,7 +49,7 @@ def run():
     app.Hide()
     avDoc = Dispatch('AcroExch.AVDoc')
     for file_path in results:
-        results.text(f'Converting File to PDF: {file_path.name}')
+        results.text(f'Processing: {file_path.name}')
         avDoc.Open(file_path, file_path)
         pdDoc = avDoc.GetPDDoc()
         target_path = file_path.with_suffix('.pdf')
@@ -65,7 +65,9 @@ def run():
         send2trash(file_path)
 
 if __name__ == '__main__':
-    print(f'Running {Path(__file__).parent.name}')
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
     start_time = datetime.now()
     run()
     end_time = datetime.now()
