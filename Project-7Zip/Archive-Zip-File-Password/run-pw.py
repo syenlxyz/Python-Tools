c42c7a9788775b53453782ac0e67dfeee13961b6
Syen Yang Lu <syenlxyz@gmail.com>
2025-11-10 17:37:48 +0800

    Create Archive-Zip-File-Password

---
+++ Project-7Zip/Archive-Zip-File-Password/run-pw.py
@@ -0,0 +1,72 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+from send2trash import send2trash
+import subprocess
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        output_path.mkdir()
+    else:
+        send2trash(output_path)
+        output_path.mkdir()
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    file_list = []
+    folder_list = []
+    path_list = list(input_path.iterdir())
+    for path in path_list:
+        if path.is_file():
+            file_list.append(path)
+        if path.is_dir():
+            folder_list.append(path)
+    
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text(f'Processing {input_path.name}: done'),
+        **options
+    )
+    for file_path in results:
+        results.text(f'Processing {input_path.name}: {file_path.name}')
+        target_path = output_path / file_path.stem
+        password = 'password'
+        subprocess.run(f'7z a -bso0 -bsp0 -tzip -p{password} "{target_path}" "{file_path}"')
+    
+    for folder_path in folder_list:
+        file_list = list(folder_path.iterdir())
+        results = alive_it(
+            file_list, 
+            len(file_list), 
+            finalize=lambda bar: bar.text(f'Processing {folder_path.name}: done'),
+            **options
+        )
+        
+        for file_path in results:
+            results.text(f'Processing {folder_path.name}: {file_path.name}')
+            target_path = output_path / folder_path.name
+            password = folder_path.name
+            for file_path in file_list:
+                subprocess.run(f'7z a -bso0 -bsp0 -tzip -p{password} "{target_path}" "{file_path}"')
+
+if __name__ == '__main__':
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
