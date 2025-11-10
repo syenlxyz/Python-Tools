5e871df30db5d94157c76955992f9f10dd602196
Syen Yang Lu <syenlxyz@gmail.com>
2025-11-10 22:14:56 +0800

    Create Add-Suffix

---
+++ Project-Pathlib/Add-Suffix/run.py
@@ -0,0 +1,70 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+from send2trash import send2trash
+import shutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        shutil.copytree(str(input_path), str(output_path))
+    else:
+        send2trash(output_path)
+        shutil.copytree(str(input_path), str(output_path))
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    file_list = []
+    folder_list = []
+    path_list = list(output_path.iterdir())
+    for path in path_list:
+        if path.is_file():
+            file_list.append(path)
+        if path.is_dir():
+            folder_list.append(path)
+    
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text(f'Processing {output_path.name}: done'),
+        **options
+    )
+    for file_path in results:
+        results.text(f'Processing {output_path.name}: {file_path.name}')
+        suffix = '-SUFFIX'
+        target_path = output_path / f'{file_path.stem}{suffix}{file_path.suffix}'
+        file_path.rename(target_path)
+    
+    for folder_path in folder_list:
+        file_list = list(folder_path.iterdir())
+        results = alive_it(
+            file_list, 
+            len(file_list), 
+            finalize=lambda bar: bar.text(f'Processing {folder_path.name}: done'),
+            **options
+        )
+        for file_path in results:
+            results.text(f'Processing {folder_path.name}: {file_path.name}')
+            suffix = str(folder_path.name).split('^')[-1]
+            target_path = folder_path / f'{file_path.stem}{suffix}{file_path.suffix}'
+            file_path.rename(target_path)
+
+if __name__ == '__main__':
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
