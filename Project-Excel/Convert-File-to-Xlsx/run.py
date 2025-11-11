1934bbd40601bd0b99d1c2b17d06e55c9ddf11b8
Syen Yang Lu <syenlxyz@gmail.com>
2025-11-11 13:57:51 +0800

    Create run.py

---
+++ Project-Excel/Convert-File-to-Xlsx/run.py
@@ -0,0 +1,57 @@
+from alive_progress import alive_it
+from datetime import datetime
+from pathlib import Path
+from send2trash import send2trash
+from win32com.client import Dispatch
+import shutil
+
+def run():
+    input_path = Path.cwd() / 'input'
+    if not input_path.is_dir():
+        input_path.mkdir()
+    
+    output_path = Path.cwd() / 'output'
+    if not output_path.is_dir():
+        shutil.copytree(str(input_path), str(output_path))
+    else:
+        send2trash(output_path)
+        shutil.copytree(str(input_path), str(output_path))
+    
+    options = {
+        'length': 70,
+        'spinner': 'classic',
+        'bar': 'classic2',
+        'receipt_text': True,
+        'dual_line': True
+    }
+    
+    file_list = list(output_path.glob('**/*.xls'))
+    results = alive_it(
+        file_list, 
+        len(file_list), 
+        finalize=lambda bar: bar.text('Processing: done'),
+        **options
+    )
+    
+    xl = Dispatch('Excel.Application')
+    xl.Visible = False
+    for file_path in results:
+        results.text(f'Processing: {file_path.name}')
+        xl.Workbooks.Open(str(file_path))
+        target_path = file_path.with_suffix('.xlsx')
+        xl.ActiveWorkbook.SaveAs(str(target_path), FileFormat=61)
+        xl.ActiveWorkbook.Close(SaveChanges=False)
+    xl.Quit()
+    
+    for file_path in file_list:
+        send2trash(file_path)
+
+if __name__ == '__main__':
+    package = Path(__file__).parent
+    module = Path(__file__)
+    print(f'Running {package.name}/{module.name}')
+    start_time = datetime.now()
+    run()
+    end_time = datetime.now()
+    run_time = end_time - start_time
+    print(f'Execution time: {run_time}')
